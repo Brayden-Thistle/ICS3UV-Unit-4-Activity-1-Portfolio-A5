8d1dc768b097b4c1b9ffaafc487e0074e8d2c3ac
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-12-15 15:20:57 +0000

    added a program that prompts the user for a range of numbers and then displays them with their ASCII value

---
+++ main.ts
@@ -0,0 +1,21 @@
+/**
+ * @author Brayden Thistle
+ * @version 1.0.0
+ * @date 2025-12-15
+ * @fileoverview this program prompts the user for a range of numbers and it displays the ASCII number beside it.
+*/
+
+//variables
+let startnumber: number;
+let endnumber: number;
+
+//declaring the variables
+startnumber = Number(prompt("Please enter a number larger than 32, but less than 126:"));
+endnumber = Number(prompt(`please enter a number larger than ${startnumber} and less than 126`))
+
+//loop and output
+for (let currentnumber = startnumber; currentnumber <= endnumber; currentnumber++) {
+  console.log(`${currentnumber} = ${String.fromCharCode(currentnumber)}`);
+}
+
+console.log("\nDone");
